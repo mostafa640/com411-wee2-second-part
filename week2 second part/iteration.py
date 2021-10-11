97bad87d4b850d8f31a87f43945e6b757319121a
mostafa640 <91554099+mostafa640@users.noreply.github.com>
2021-10-11 15:16:49 +0100

    while cable

---
+++ week2 second part/iteration.py
@@ -0,0 +1,7 @@
+print("How many cables should I remove?")
+cables_remove = int(input())
+cables_removed = 0
+print()
+while cables_removed < cables_remove:
+    print("Removed cable.")
+    cables_removed += 1
